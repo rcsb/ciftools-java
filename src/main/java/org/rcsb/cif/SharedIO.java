f1d6957743068205cfa2aa3749d5a18f04cc2a83
Sebastian Bittrich <bittrich@hs-mittweida.de>
2019-05-03 11:17:04 -0700

    adds fetch URLs

--- src/main/java/org/rcsb/cif/SharedIO.java
+++ src/main/java/org/rcsb/cif/SharedIO.java
@@ -1,12 +1,12 @@
 package org.rcsb.cif;
 
-import java.io.BufferedInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 public class SharedIO {
     private static final int DEFAULT_BUFFER_SIZE = 65536;
+    // TODO change once ready
+    private static final String DEFAULT_BCIF_FETCH_URL = "https://webchem.ncbr.muni.cz/ModelServer/static/bcif/%s";
+    private static final String DEFAULT_CIF_FETCH_URL = "https://files.rcsb.org/download/%s.cif.gz";
 
     public static byte[] inputStreamToBytes(InputStream inputStream) throws IOException {
         // performance: explicitly buffer stream, increases performance drastically
@@ -14,12 +14,11 @@ public class SharedIO {
             inputStream = new BufferedInputStream(inputStream, DEFAULT_BUFFER_SIZE);
         }
 
-        // TODO 2nd layer of buffering needed?
         ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-        int nRead;
+        int bytesRead;
         byte[] data = new byte[DEFAULT_BUFFER_SIZE];
-        while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
-            buffer.write(data, 0, nRead);
+        while ((bytesRead = inputStream.read(data, 0, data.length)) != -1) {
+            buffer.write(data, 0, bytesRead);
         }
 
         buffer.flush();
