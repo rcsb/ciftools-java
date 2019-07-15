4bc13571a7498151fcc4b36255547a1edbc7c5a4
Sebastian Bittrich <bittrich@hs-mittweida.de>
2019-07-15 10:55:27 -0700

    footprint analysis

--- src/test/java/org/rcsb/cif/FootprintTest.java
+++ src/test/java/org/rcsb/cif/FootprintTest.java
@@ -4,21 +4,29 @@ import org.junit.Test;
 import org.rcsb.cif.binary.codec.MessagePackCodec;
 
 import java.io.IOException;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static org.junit.Assert.*;
+
 public class FootprintTest {
     @Test
     public void printFootprint() throws IOException {
-        byte[] bytes = CifIO.writeBinary(CifIO.readFromInputStream(TestHelper.getInputStream("cif/1acj.cif")));
-//        printFootprint(TestHelper.getBytes("source/1acj_ebi.bcif"));
-//        printFootprint(TestHelper.getBytes("source/1acj_java.bcif"));
-        printFootprint(bytes);
+        byte[] bytes = CifIO.writeBinary(CifIO.readFromInputStream(TestHelper.getInputStream("source/200l_rcsb.cif")));
+        Map<String, String> writtenFootprint = getFootprint(bytes);
+
+        Map<String, String> ebiFootprint = getFootprint(TestHelper.getBytes("source/200l_ebi.bcif"));
+        Map<String, String> molstarFootprint = getFootprint(TestHelper.getBytes("source/200l_molstar.bcif"));
+
+        equals(ebiFootprint, writtenFootprint);
+        equals(molstarFootprint, writtenFootprint);
     }
 
     @SuppressWarnings("unchecked")
-    private void printFootprint(byte[] content) {
+    private Map<String, String> getFootprint(byte[] content) {
+        Map<String, String> footprint = new LinkedHashMap<>();
         MessagePackCodec messagePackCodec = new MessagePackCodec();
         Object[] file = (Object[]) messagePackCodec.decode(content).get("dataBlocks");
         Map<String, Object> firstBlock = (Map<String, Object>) file[0];
@@ -26,20 +34,40 @@ public class FootprintTest {
         for (Object cat : categories) {
             Map<String, Object> category = (Map<String, Object>) cat;
             String categoryName = (String) category.get("name");
-            if (categoryName.startsWith("_coordinate")) {
+            // coordinate server categories are not present
+            if (categoryName.startsWith("_coordinate") || categoryName.equals("_chem_comp_bond")) {
                 continue;
             }
-            System.out.println(categoryName);
 
             Object[] columns = (Object[]) category.get("columns");
             for (Object col : columns) {
                 Map<String, Object> column = (Map<String, Object>) col;
                 String columnName = (String) column.get("name");
-                System.out.println(categoryName + "." + columnName);
+                String key = categoryName + "." + columnName;
+                // some column are not present
+                if (key.equals("_struct_sheet_range.symmetry") || key.equals("_symmetry.space_group_name_Hall") ||
+                        key.equals("_atom_site.pdbe_label_seq_id")) {
+                    continue;
+                }
 
                 Map<String, Object> data = (Map<String, Object>) column.get("data");
                 Object[] enc = (Object[]) data.get("encoding");
-                System.out.println(enc.length + "," + category.get("rowCount") + "," + Stream.of(enc).map(e -> ((Map<String, Object>) e).get("kind")).collect(Collectors.toList()));
+
+                String value = category.get("rowCount") + "," + enc.length + "," + Stream.of(enc).map(e -> ((Map<String, Object>) e).get("kind")).collect(Collectors.toList());
+                footprint.put(key, value);
+            }
+        }
+        return footprint;
+    }
+
+    private void equals(Map<String, String> expected, Map<String, String> actual) {
+        for (String key : expected.keySet()) {
+            if (!actual.containsKey(key)) {
+                fail("key " + key + " not present");
+            }
+
+            if (!expected.get(key).equals(actual.get(key))) {
+                System.out.println("mismatch in column " + key + "\n" + expected.get(key) + " vs " + actual.get(key));
             }
         }
     }
