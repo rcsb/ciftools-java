ea06d2acca487956db944c37ed75214c1425a631
Sebastian Bittrich <sebastian.bittrich@rcsb.org>
2021-09-23 09:51:13 -0700

    promote PdbxNonpolyNdbSeqNumTest to integration test

--- src/test/java/org/rcsb/cif/IntegrationTest.java
+++ src/test/java/org/rcsb/cif/IntegrationTest.java
@@ -307,4 +307,40 @@ public class IntegrationTest {
 
         assertEqualsIgnoringWhitespaces(originalContent, copyContent);
     }
+
+    /**
+     * BinaryCIF might slightly violate the schema and lead to a ClassCastException if not handled.
+     */
+    @Test
+    public void readRcsbAndEbiGeneric() throws IOException {
+        CifFile rcsb = CifIO.readById("1acj");
+        Column<?> rcsbNdbSeqNum = rcsb.getBlocks().get(0)
+                .getCategory("pdbx_nonpoly_scheme")
+                .getColumn("ndb_seq_num");
+        assertEquals(83, rcsbNdbSeqNum.getRowCount());
+
+        CifFile ebi = CifIO.readById("1acj",
+                new CifOptions.CifOptionsBuilder().fetchUrl("https://www.ebi.ac.uk/pdbe/coordinates/%s/full?encoding=bcif").build());
+        Column<?> ebiNdbSeqNum = ebi.getBlocks().get(0)
+                .getCategory("pdbx_nonpoly_scheme")
+                .getColumn("ndb_seq_num");
+        assertEquals(83, ebiNdbSeqNum.getRowCount());
+    }
+
+    @Test
+    public void readRcsbAndEbiWithSchema() throws IOException {
+        MmCifFile rcsb = CifIO.readById("1acj").as(StandardSchemata.MMCIF);
+        StrColumn rcsbNdbSeqNum = rcsb.getFirstBlock()
+                .getPdbxNonpolyScheme()
+                .getNdbSeqNum();
+        assertEquals(83, rcsbNdbSeqNum.getRowCount());
+
+        MmCifFile ebi = CifIO.readById("1acj",
+                        new CifOptions.CifOptionsBuilder().fetchUrl("https://www.ebi.ac.uk/pdbe/coordinates/%s/full?encoding=bcif").build())
+                .as(StandardSchemata.MMCIF);
+        StrColumn ebiNdbSeqNum = ebi.getFirstBlock()
+                .getPdbxNonpolyScheme()
+                .getNdbSeqNum();
+        assertEquals(83, ebiNdbSeqNum.getRowCount());
+    }
 }
